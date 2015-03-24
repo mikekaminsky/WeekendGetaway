6852f60e04bfa014d3427773cfb12d7846f89e46
mikekaminsky <mkaminsky@casecommons.org>
2015-03-24 19:54:16 -0400

    Pushing queried information into DB. Looping over airports

--- pullflights.py
+++ pullflights.py
@@ -3,61 +3,97 @@
 
 import json
 import requests
-import pandas
 import time
+import datetime
+import psycopg2
+import os
 
-date_time = time.strftime("%Y/%m/%d %H:%M:%S")
-
-#airport_list = ["ATL", "PEK", "LHR", "HND", "ORD"]
-
+#Database
+db_url = 'weekendgetaway'
+username = os.environ['USER']
+host = 'localhost'
+con = psycopg2.connect(dbname=db_url, user=username, host=host)
+cur = con.cursor()
 execfile("secrets.py") # declares api_key variable
 
-departure_date = "2015-09-19"
-return_date = "2015-09-21"
-origin_city = "NYC"
-destination_city = "MDE"
-
-
+# Constants
 url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + api_key
 headers = {'content-type': 'application/json'}
+airport_list = ["ATL", "PEK", "LHR", "HND", "ORD"]
+origin_city = "NYC"
+departure_date = "2015-03-27"
+return_date = str(datetime.datetime.strptime(departure_date,"%Y-%m-%d").date() + datetime.timedelta(days = 3))
 
-params = {
-  "request": {
-    "slice": [
-      {
-        "origin": origin_city,
-        "destination": destination_city,
-        "date": departure_date
-      },
-      {
-        "origin": destination_city,
-        "destination": origin_city,
-        "date": return_date
+for destination_city in airport_list:
+    print origin_city
+    print destination_city
+
+    params = {
+      "request": {
+        "slice": [
+          {
+            "origin": origin_city,
+            "destination": destination_city,
+            "date": departure_date
+          },
+          {
+            "origin": destination_city,
+            "destination": origin_city,
+            "date": return_date
+          }
+        ],
+        "passengers": {
+          "adultCount": 1
+        },
       }
-    ],
-    "passengers": {
-      "adultCount": 1
-    },
-  }
-}
+    }
 
+    date_time = time.strftime("%Y/%m/%d %H:%M:%S")
+    response = requests.post(url, data=json.dumps(params), headers=headers)
+    options = response.json()['trips']['tripOption']
 
+    #Check if row is already in the database
+    #If not, add it
+    cur.execute("""
+            SELECT id 
+            FROM trips 
+            WHERE 
+                origin_city = (%s) 
+                AND destination_city = (%s) 
+                AND departure_date = (%s)
+                AND return_date = (%s)""", (origin_city, destination_city, departure_date, return_date))
+    cur_id_tuple = cur.fetchall()
+    if len(cur_id_tuple) != 0:
+        cur_id = cur_id_tuple[0][0]
+    else:
+        cur.execute("""
+            INSERT INTO trips (origin_city, destination_city, departure_date, return_date) 
+            VALUES (%s, %s, %s, %s)
+            RETURNING id""", (origin_city, destination_city, departure_date, return_date))
+        cur_id = cur.fetchall()
+        cur_id = cur_id[0][0]
 
-
-for option in options:
-    total_cost = option['saleTotal']
-    print total_cost
-    for eachslice in option['slice']:
-        for segment in eachslice['segment']:
-            segment_travel_time = segment['duration']
-            carrier = segment['flight']['carrier']
-            number = segment['flight']['number']
-            leg = segment['leg'][0] # it looks like there's only one leg per segment?!
-            leg_origin = leg['origin']
-            leg_departure = leg['departureTime']
-            leg_destination = leg['destination']
-            leg_arrival = leg['arrivalTime']
-            leg_duration = leg['duration']
-            print carrier + number + " " + leg_origin + " -> " + leg_destination + " " + str(leg_duration) + " minutes"
-
-
+    for option in options:
+        total_cost = option['saleTotal']
+        cur.execute("""
+            INSERT INTO flights (trip_id, time_queried, price) 
+            VALUES (%s, %s, %s) returning id
+            """, (cur_id, date_time, total_cost))
+        flight_id = cur.fetchall()
+        flight_id = flight_id[0][0]
+        for eachslice in option['slice']: # Here is where we determine inbound vs. outbound slices.
+            for segment in eachslice['segment']:
+                segment_travel_time = segment['duration']
+                carrier = segment['flight']['carrier']
+                number = segment['flight']['number']
+                leg = segment['leg'][0] # I think we need to make this an explicit loop!
+                leg_origin = leg['origin']
+                leg_departure = leg['departureTime']
+                leg_destination = leg['destination']
+                leg_arrival = leg['arrivalTime']
+                leg_duration = leg['duration']
+                cur.execute("""
+                    INSERT INTO legs (flight_id, carrier, flight_number, origin, departure_time, destination, arrival_time, duration) 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    RETURNING id""", (flight_id, carrier, number, leg_origin, leg_departure, leg_destination, leg_arrival, leg_duration))
+                con.commit()
