c4adacb2d2371746e72483a63b0071d28abec67b
Michael <ccUser@cc-notebook-19.home>
2015-04-05 16:43:48 -0400

    Using data in DB to define which flights to query.

--- Execute.py
+++ Execute.py
@@ -1,24 +1,32 @@
 #Execute.py
 #Michael Kaminsky
-
 import datetime
 import PullFlights
 from db.Connection import *
+from db.models import *
 
 # Inputs
-#destination_list = ["AUS", "PHX", "LAX"]
-destination_list = ["AUS"]
-origin_list = ["NYC"]
+origin_list = []
+origin_query = session.query(Origin).distinct(Origin.origin)
+for x in origin_query:
+    origin_list.append(x.origin )
 
-#departure_dates = ["2015-05-01", "2015-05-08"]
-departure_dates = ["2015-05-01"]
+destination_list  = []
+destination_query = session.query(Destination).distinct(Destination.destination)
+for x in destination_query:
+    destination_list.append(x.destination)
+
+departure_dates   = []
+departure_query = session.query(Departure).distinct(Departure.departure_date)
+for x in departure_query:
+    departure_dates.append(x.departure_date)
 
 # Generate return dates for our departure dates list
 date_list = []
 for date in departure_dates:
-  return_date = str(datetime.datetime.strptime(date,"%Y-%m-%d").date() + datetime.timedelta(days = 3))
-  date_pair = (date, return_date)
-  date_list.append(date_pair)
+    return_date = str(date + datetime.timedelta(days = 3))
+    date_pair = (date, return_date)
+    date_list.append(date_pair)
 
 pf = PullFlights.PullFlights(session, origin_list, destination_list, date_list, api_key)
 pf.UpdateData()
